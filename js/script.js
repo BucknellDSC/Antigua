2ee8953bd18f75d5beba8cc4d4cdafb47a6db922
Khoi Le <kal037@bucknell.edu>
2017-10-13 16:22:46 -0400

    svg parish

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
-window.onload = function () {
-    var test_img = document.getElementById('test-img');
-    var n,
-        areas = document.getElementById('test-map').getElementsByTagName('area'),
-        len = areas.length,
-        coords = [],
-        previousWidth = test_img.width;
-    for (n = 0; n < len; n++) {
-        coords[n] = areas[n].coords.split(',');
-    }
-
-    test_img.height = window.innerHeight;
-    test_img.width = window.innerWidth;
-
-    console.log(test_img.offsetWidth);
-
-    console.log(test_img.width);
-    var m, clen,
-        x = test_img.offsetWidth / previousWidth;
-    for (n = 0; n < len; n++) {
-        clen = coords[n].length;
-        for (m = 0; m < clen; m++) {
-            coords[n][m] *= x;
-        }
-        areas[n].coords = coords[n].join(',');
-    }
-    console.log(areas[0]);
-};
+$("#test-path").on('click', function() {
+    console.log("eyyy");
+    window.location.href = "stgeorge.html"
+});
