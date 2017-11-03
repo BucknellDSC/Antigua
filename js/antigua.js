2a1b4db342593fd2a9b99cc3323e1214ca92da67
khaiquangnguyen <kqn001@bucknell.edu>
2017-11-03 15:25:30 -0400

    done

--- js/antigua.js
+++ js/antigua.js
@@ -1,9 +1,8 @@
-
 $(document).ready(function () {
     // the list of all mills in the side panel
     var $mill_list = $('#mill_list');
     // hidden variable all_mists: a json type variable contaning information of all mills
-    
+
     // add all mills to the list of mills on the side panel
     for (i in all_mills) {
         jQuery('<div/>', {
@@ -13,31 +12,33 @@ $(document).ready(function () {
         }).appendTo($mill_list);
     }
     // switch to st.john on click
-    $("#stjohn-path").on('click', function() {
+    $("#stjohn-path").on('click', function () {
         window.location.href = "stjohn.html"
     });
-});
-
-$('.marker').bind('click', function() {
-    $('.card').addClass('active');
-    $('.marker').addClass('inactive');
-});
-
+    $('.marker').bind('click', function () {
+        $('.card').addClass('active');
+        $('.marker').addClass('inactive');
+    });
+    
 $('.card').bind('click', function() {
     $('.card').removeClass('active');
     $('.marker').removeClass('inactive');
 });
 
+});
+
+
+
 function show_full_info() {
     var modal = document.getElementsByClassName("modal")[0];
 
-// Get the <span> element that closes the modal
+    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
+    // When the user clicks the button, open the modal
     modal.style.display = "block";
 
-// When the user clicks on <span> (x), close the modal
+    // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     };
