1adb45ec63093c9d467538ecfa587dfee540c954
Khai Quang Nguyen <kqn001@bucknell.edu>
2017-11-09 00:39:39 -0500

    fix card

--- js/antigua.js
+++ js/antigua.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // the list of all mills in the side panel
   // var $mill_list = $('#mill_list');
   // // hidden variable all_mists: a json type variable contaning information of all mills
@@ -20,11 +20,11 @@ $(document).ready(function() {
   $("#stphilip_map").hide();
   $("#antigua_map").hide();
   var $current_map = $("#stjohn_map");
-
+  create_mill_marker(mill_locations);
   //   hide the dummy elements of html file. They are used as blueprint
-  $(".blueprint").hide();
+  // $(".blueprint").hide();
 
-    //   var mill_locaitons which contains all location data of mill_locaitons. declared in mill_location.json file
+  //   var mill_locaitons which contains all location data of mill_locaitons. declared in mill_location.json file
 
   /**
    * Create mill markers on a parish from a location json file
@@ -33,10 +33,10 @@ $(document).ready(function() {
   function create_mill_marker(location_array) {
     var $marker = $(".marker.blueprint");
     var new_marker;
-    for (i in location_array) {
+    for (var i in location_array) {
       new_marker = $marker.clone();
       new_marker.removeClass("blueprint");
-      var left = location_array[i].left.toString()+"%";
+      var left = location_array[i].left.toString() + "%";
       var top = location_array[i].top.toString() + "%";
       new_marker.css("left", left);
       new_marker.css("top", top);
@@ -47,12 +47,12 @@ $(document).ready(function() {
   var $card = $(".blueprint .card");
   var $modal = $(".blueprint .modal");
 
-  $(".menu_button").on("mouseover", function() {
+  $(".menu_button").on("mouseover", function () {
     $(this).css("border", " 1px ridge rgba(242, 207, 141, 1)");
   });
 
   // menu item focus
-  $(".menu_button").on("click", function() {
+  $(".menu_button").on("click", function () {
     $(".expandable_list", this).css({
       height: "30vh",
       "margin-top": "2em"
@@ -62,7 +62,7 @@ $(document).ready(function() {
     });
   });
 
-  $(".menu_button").on("mouseleave", function() {
+  $(".menu_button").on("mouseleave", function () {
     $(this).css("border", " 1px ridge transparent");
     $(".expandable_list", this).css({
       height: "0vh",
@@ -74,43 +74,43 @@ $(document).ready(function() {
   });
 
   // switch to st.john on click
-  $("#stjohn-path").on("click", function() {
+  $("#stjohn-path").on("click", function () {
     // window.location.href = "stjohn.html"
     $("#stjohn_map").fadeIn();
     $("#antigua_map").fadeOut();
   });
 
-  $("#stpaul-path").on("click", function() {
+  $("#stpaul-path").on("click", function () {
     window.location.href = "stpaul.html";
   });
 
-  $("#stpeter-path").on("click", function() {
+  $("#stpeter-path").on("click", function () {
     window.location.href = "stpeter.html";
   });
 
-  $("#stphilip-path").on("click", function() {
+  $("#stphilip-path").on("click", function () {
     window.location.href = "stphilip.html";
   });
 
-  $("#stmary-path").on("click", function() {
+  $("#stmary-path").on("click", function () {
     window.location.href = "stmary.html";
   });
 
-  $("#stgeorge-path").on("click", function() {
+  $("#stgeorge-path").on("click", function () {
     window.location.href = "stgeorge.html";
   });
 
-  $(".marker").bind("click", function() {
+  $(".marker").bind("click", function () {
     $(".card").addClass("active");
     $(".marker").addClass("inactive");
   });
 
-  $(".card").bind("click", function() {
+  $(".card").bind("click", function () {
     $(".card").removeClass("active");
     $(".marker").removeClass("inactive");
   });
 
-  $("#home_button").on("click", function() {
+  $("#home_button").on("click", function () {
     if ($("#middle-slide").hasClass("active")) {
       $("#middle-slide").removeClass("active");
     } else {
@@ -134,7 +134,7 @@ function show_full_info() {
   modal.style.display = "block";
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+  span.onclick = function () {
     modal.style.display = "none";
   };
 }
