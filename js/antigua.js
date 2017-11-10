e1643d059d7257a6f9ad25196320d19d93d632d8
Khai Quang Nguyen <kqn001@bucknell.edu>
2017-11-09 21:30:16 -0500

    done

--- js/antigua.js
+++ js/antigua.js
@@ -1,30 +1,95 @@
-$(document).ready(function () {
-  // the list of all mills in the side panel
-  // var $mill_list = $('#mill_list');
-  // // hidden variable all_mists: a json type variable contaning information of all mills
-  // // add all mills to the list of mills on the side panel
-  //   for (i in all_mills) {
-  //       jQuery('<div/>', {
-  //           id: all_mills[i].name,
-  //           class: "menu_item",
-  //           text: all_mills[i].name,
-  //       }).appendTo($mill_list);
-  //   }
+$(document).ready(function() {
+  // -------------- VARIABLE DECLARATION --------------------
 
-  // hide the parishes map
-  // $("#stjohn_map").hide();
+  // the map which is currently active and show on the screen. jquery object
+  var $current_map;
+  // The name of the current map. Use for functions which require current map information
+  // possible value: stgeorge|stmary|stpaul|stpeter|stphilip|stjohn|antigua
+  var current_map;
+  // the array of all parishes
+  var parishes_array = [];
+  // the aray of all mills
+  var mills_array = [];
+  // the dictionary of all mills, sorted according to parishes
+  var mills_by_parishes_array = {};
+  // the array of all mills we are currently working with
+  var current_mills_array = [];
+  //   var mill_locaitons, which contains all location data of mills. Declared in mill_location.json file
+  // var mill_data, which contains all data of mills. Declared in mill_data.json file
+  var mill_data = all_mills;
+  // -------------- INIT CALLS. RUN WHEN WEBSITE STARTS ---------------------
+
+  // show main map and hide other maps. We have to do this because they live on the same page
+  $("#stjohn_map").show();
   $("#stgeorge_map").hide();
   $("#stmary_map").hide();
   $("#stpaul_map").hide();
   $("#stpeter_map").hide();
   $("#stphilip_map").hide();
   $("#antigua_map").hide();
-  var $current_map = $("#stjohn_map");
-  create_mill_marker(mill_locations);
-  //   hide the dummy elements of html file. They are used as blueprint
-  // $(".blueprint").hide();
 
-  //   var mill_locaitons which contains all location data of mill_locaitons. declared in mill_location.json file
+  $current_map = $("#stjohn_map");
+
+  // Process mill data into different form.
+  // Specifically, we need a list of mill names | a list of parishes name | a list of mill names based on parishes
+  for (var index in mill_data) {
+    var a_mill = mill_data[index];
+    var parish = a_mill.parish;
+    var mill_name = a_mill.name;
+    mills_array.push(mill_name);
+    // if we haven't stored the parish yet, add it to parish list and mills by parishes dict
+    if (parishes_array.indexOf(parish) < 0) {
+      parishes_array.push(parish);
+      mills_by_parishes_array[parish] = [mill_name];
+    } else {
+      // else, add the mill to the dictionary of parishes
+      mills_by_parishes_array[parish].push(mill_name);
+    }
+  }
+
+  // sort the mill names inside the list
+  mills_array.sort();
+  parishes_array.sort();
+  for (var key in mills_by_parishes_array) {
+    mills_by_parishes_array[key].sort();
+  }
+
+  // add parishes to menu
+  var $parish_list = $("#parish_list");
+  for (var a_parish in mills_by_parishes_array) {
+    jQuery("<div/>", {
+      id: a_parish,
+      class: "menu_list_item",
+      text: a_parish
+    }).appendTo($parish_list);
+  }
+
+  // start with main map, current list of mills is list of all mills
+  current_mills_array = mills_array;
+
+  // add mills to menu. start with all mills
+  add_mills_to_menu();
+
+  // ------------------ MAIN FUNCTIONS --------------------------
+
+  /**
+   * Add list of mills to menu based on the current map. 
+   * Depends on global current_mills_array
+   */
+  function add_mills_to_menu() {
+    // get element which contains the list in html
+    var $mill_list = $("#mill_list");
+    // add list of mills to html element
+    for (mill_name of current_mills_array) {
+      jQuery("<div/>", {
+        id: name,
+        class: "menu_list_item",
+        text: name
+      }).appendTo($mill_list);
+    }
+  }
+
+  create_mill_marker(mill_locations);
 
   /**
    * Create mill markers on a parish from a location json file
@@ -47,12 +112,12 @@ $(document).ready(function () {
   var $card = $(".blueprint .card");
   var $modal = $(".blueprint .modal");
 
-  $(".menu_button").on("mouseover", function () {
+  $(".menu_button").on("mouseover", function() {
     $(this).css("border", " 1px ridge rgba(242, 207, 141, 1)");
   });
 
   // menu item focus
-  $(".menu_button").on("click", function () {
+  $(".menu_button").on("click", function() {
     $(".expandable_list", this).css({
       height: "30vh",
       "margin-top": "2em"
@@ -62,7 +127,7 @@ $(document).ready(function () {
     });
   });
 
-  $(".menu_button").on("mouseleave", function () {
+  $(".menu_button").on("mouseleave", function() {
     $(this).css("border", " 1px ridge transparent");
     $(".expandable_list", this).css({
       height: "0vh",
@@ -74,43 +139,43 @@ $(document).ready(function () {
   });
 
   // switch to st.john on click
-  $("#stjohn-path").on("click", function () {
+  $("#stjohn-path").on("click", function() {
     // window.location.href = "stjohn.html"
     $("#stjohn_map").fadeIn();
     $("#antigua_map").fadeOut();
   });
 
-  $("#stpaul-path").on("click", function () {
+  $("#stpaul-path").on("click", function() {
     window.location.href = "stpaul.html";
   });
 
-  $("#stpeter-path").on("click", function () {
+  $("#stpeter-path").on("click", function() {
     window.location.href = "stpeter.html";
   });
 
-  $("#stphilip-path").on("click", function () {
+  $("#stphilip-path").on("click", function() {
     window.location.href = "stphilip.html";
   });
 
-  $("#stmary-path").on("click", function () {
+  $("#stmary-path").on("click", function() {
     window.location.href = "stmary.html";
   });
 
-  $("#stgeorge-path").on("click", function () {
+  $("#stgeorge-path").on("click", function() {
     window.location.href = "stgeorge.html";
   });
 
-  $(".marker").bind("click", function () {
+  $(".marker").bind("click", function() {
     $(".card").addClass("active");
     $(".marker").addClass("inactive");
   });
 
-  $(".card").bind("click", function () {
+  $(".card").bind("click", function() {
     $(".card").removeClass("active");
     $(".marker").removeClass("inactive");
   });
 
-  $("#home_button").on("click", function () {
+  $("#home_button").on("click", function() {
     if ($("#middle-slide").hasClass("active")) {
       $("#middle-slide").removeClass("active");
     } else {
@@ -125,7 +190,7 @@ $(document).ready(function () {
 });
 
 function show_full_info() {
-  var modal = document.getElementsByClassName("modal")[0];
+  var modal = $(".modal");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
@@ -134,7 +199,7 @@ function show_full_info() {
   modal.style.display = "block";
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
+  span.onclick = function() {
     modal.style.display = "none";
   };
 }
