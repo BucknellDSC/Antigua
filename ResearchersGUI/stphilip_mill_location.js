18b375061446fc17f9e9e6b2df919583683ed175
patricknewhart <32399982+patricknewhart@users.noreply.github.com>
2017-11-25 14:44:02 -0500

    Update St. Philips Locations
    Check:
    94, 127, 129

--- ResearchersGUI/stphilip_mill_location.js
+++ ResearchersGUI/stphilip_mill_location.js
@@ -1,187 +1,187 @@
 var stphilip_mill_locations = [
   {
-      "left": 0,
+      "left": 30,
       "name": "Christian Hill  Cussacks",
-      "top": 0
+      "top": 13
   },
   {
-      "left": 0,
+      "left": 60,
       "name": "Rooms",
-      "top": 0
+      "top": 7
   },
   {
-      "left": 0,
+      "left": 36,
       "name": "Parsons Maul  Parson Maules",
-      "top": 0
+      "top": 10
   },
   {
-      "left": 0,
+      "left": 32,
       "name": "Glanvilles",
-      "top": 0
+      "top": 18
   },
   {
-      "left": 0,
+      "left": 29,
       "name": "Collins",
-      "top": 0
+      "top": 30
   },
   {
-      "left": 0,
+      "left": 45,
       "name": "Grants",
-      "top": 0
+      "top": 17
   },
   {
-      "left": 0,
+      "left": 40,
       "name": "Jefferson  Zion Hill",
-      "top": 0
+      "top": 21
   },
   {
-      "left": 0,
+      "left": 54,
       "name": "Mayers  Benlomand",
-      "top": 0
+      "top": 18
   },
   {
-      "left": 0,
+      "left": 70,
       "name": "Retreat  Montgomery",
-      "top": 0
+      "top": 13
   },
   {
-      "left": 0,
+      "left": 51,
       "name": "Comfort Hall",
-      "top": 0
+      "top": 25
   },
   {
-      "left": 0,
+      "left": 59,
       "name": "Grays Belfast  Lambert Hall",
-      "top": 0
+      "top": 27
   },
   {
-      "left": 0,
+      "left": 45,
       "name": "Wickhams  Upper Freemans",
-      "top": 0
+      "top": 30
   },
   {
-      "left": 0,
+      "left": 26,
       "name": "Elliott  Frenchs",
-      "top": 0
+      "top": 25
   },
   {
-      "left": 0,
+      "left": 19,
       "name": "Lon Lane",
-      "top": 0
+      "top": 40
   },
   {
-      "left": 0,
+      "left": 28,
       "name": "Gaynors",
-      "top": 0
+      "top": 42
   },
   {
-      "left": 0,
+      "left": 32,
       "name": "The Grange",
-      "top": 0
+      "top": 43
   },
   {
-      "left": 0,
+      "left": 30,
       "name": "Elme Creek",
-      "top": 0
+      "top": 49
   },
   {
-      "left": 0,
+      "left": 40,
       "name": "Goble  Gables",
-      "top": 0
+      "top": 47
   },
   {
-      "left": 0,
+      "left": 50,
       "name": "Montpelie  Murrays",
-      "top": 0
+      "top": 80
   },
   {
-      "left": 0,
+      "left": 48,
       "name": "Archbolds",
-      "top": 0
+      "top": 50
   },
   {
-      "left": 0,
+      "left": 59,
       "name": "Brown Bay harmony Hall",
-      "top": 0
+      "top": 45
   },
   {
-      "left": 0,
+      "left": 65,
       "name": "Skerret & Folly  Nugents",
-      "top": 0
+      "top": 49
   },
   {
-      "left": 0,
+      "left": 57,
       "name": "Colebrooks",
-      "top": 0
+      "top": 65
   },
   {
-      "left": 0,
+      "left": 48,
       "name": "Walrond Upper",
-      "top": 0
+      "top": 58
   },
   {
-      "left": 0,
+      "left": 25,
       "name": "Walrond Lower",
-      "top": 0
+      "top": 70
   },
   {
-      "left": 0,
+      "left": 30,
       "name": "Frye  Frys",
-      "top": 0
+      "top": 63
   },
   {
-      "left": 0,
+      "left": 41,
       "name": "Th Hope",
-      "top": 0
+      "top": 83
   },
   {
-      "left": 0,
+      "left": 42,
       "name": "Harman  Harmons  Lloyds",
-      "top": 0
+      "top": 70
   },
   {
-      "left": 0,
+      "left": 20,
       "name": "Lavingtons",
-      "top": 0
+      "top": 53
   },
   {
-      "left": 0,
+      "left": 54,
       "name": "Lynchs",
-      "top": 0
+      "top": 87
   },
   {
-      "left": 0,
+      "left": 25,
       "name": "Lyon (Upper & Lower)",
-      "top": 0
+      "top": 55
   },
   {
-      "left": 0,
+      "left": 65,
       "name": "Mannings",
-      "top": 0
+      "top": 76
   },
   {
-      "left": 0,
+      "left": 73,
       "name": "Sheriff (Exchange)",
-      "top": 0
+      "top": 65
   },
   {
-      "left": 0,
+      "left": 79,
       "name": "Watsons",
-      "top": 0
+      "top": 60
   },
   {
-      "left": 0,
+      "left": 55,
       "name": "Gree Island",
-      "top": 0
+      "top": 55
   },
   {
-      "left": 0,
+      "left": 75,
       "name": "Harr Hardings",
-      "top": 0
+      "top": 50
   },
   {
-      "left": 0,
+      "left": 59,
       "name": "Th Grange",
-      "top": 0
+      "top": 55
   }
 ]
