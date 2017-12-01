5b2356fe47ff15b9e7bcd081fea1bf3b77060117
Khai Quang Nguyen <kqn001@bucknell.edu>
2017-12-01 13:05:19 -0500

    fix chronos

--- js/antigua.js
+++ js/antigua.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // -------------- VARIABLE DECLARATION --------------------
 
   // the map which is currently active and show on the screen. jquery object
@@ -23,15 +23,18 @@ $(document).ready(function () {
   /**
    * Menu button styling on mouse over
    */
-  $(".menu_button").on("mouseover", function () {
+  $(".menu_button").on("mouseover", function() {
     $(this).css("border", " 1px ridge rgba(242, 207, 141, 1)");
   });
 
   // menu item focus
-  $(".menu_button").on("click", function () {
-    var distance_to_bottom = $(window).height() - $('#credit_button')[0].getBoundingClientRect().bottom - $('#credit_button').outerHeight();
+  $(".menu_button").on("click", function() {
+    var distance_to_bottom =
+      $(window).height() -
+      $("#credit_button")[0].getBoundingClientRect().bottom -
+      $("#credit_button").outerHeight();
     $(".expandable_list", this).css({
-      "height": distance_to_bottom.toString(),
+      height: distance_to_bottom.toString(),
       "margin-top": "2em"
     });
     if ($(".menu_title", this).attr("id") !== "info_button") {
@@ -50,7 +53,7 @@ $(document).ready(function () {
   /**
    * Menu button styling on mouse over
    */
-  $(".menu_button").on("mouseleave", function () {
+  $(".menu_button").on("mouseleave", function() {
     $(this).css("border", " 1px ridge transparent");
     $(".expandable_list", this).css({
       height: "0vh",
@@ -64,7 +67,7 @@ $(document).ready(function () {
   /**
    * What to do when info button is clicked. Doesn't require any data from .js file
    */
-  $("#info_button").on("click", function () {
+  $("#info_button").on("click", function() {
     if (!$("#middle-slide").hasClass("active")) {
       $("#middle-slide").addClass("active");
       $("#middle-slide-title").addClass("active");
@@ -75,7 +78,6 @@ $(document).ready(function () {
       $("#middle-slide-content").removeClass("active");
     }
   });
-
 
   // -------------- INIT CALLS. RUN WHEN WEBSITE STARTS ---------------------
 
@@ -95,15 +97,18 @@ $(document).ready(function () {
     var a_mill = mill_data[index];
     var parish = a_mill.parish;
     var mill_name = a_mill.name;
-    mills_array.push(mill_name);
-    new_mill_data[mill_name] = a_mill;
-    // if we haven't stored the parish yet, add it to parish list and mills by parishes dict
-    if (parishes_array.indexOf(parish) < 0) {
-      parishes_array.push(parish);
-      mills_by_parishes_array[parish] = [mill_name];
-    } else {
-      // else, add the mill to the dictionary of parishes
-      mills_by_parishes_array[parish].push(mill_name);
+    // only add in if mill have enough information
+    if (parish != "") {
+      mills_array.push(mill_name);
+      new_mill_data[mill_name] = a_mill;
+      // if we haven't stored the parish yet, add it to parish list and mills by parishes dict
+      if (parishes_array.indexOf(parish) < 0) {
+        parishes_array.push(parish);
+        mills_by_parishes_array[parish] = [mill_name];
+      } else {
+        // else, add the mill to the dictionary of parishes
+        mills_by_parishes_array[parish].push(mill_name);
+      }
     }
   }
 
@@ -114,6 +119,14 @@ $(document).ready(function () {
     mills_by_parishes_array[key].sort();
   }
 
+  // start with main map, current list of mills is list of all mills
+  current_mills_array = mills_array;
+
+  // add mills to menu. start with all mills
+  add_mills_to_menu();
+
+  // ------------------ MAIN FUNCTIONS --------------------------
+
   // add parishes to menu
   var $parish_list = $("#parish_list");
   for (var a_parish in mills_by_parishes_array) {
@@ -123,15 +136,6 @@ $(document).ready(function () {
       text: a_parish
     }).appendTo($parish_list);
   }
-
-  // start with main map, current list of mills is list of all mills
-  current_mills_array = mills_array;
-
-  // add mills to menu. start with all mills
-  add_mills_to_menu();
-
-
-  // ------------------ MAIN FUNCTIONS --------------------------
 
   /**
    * Add list of mills to menu based on the current map.
@@ -158,94 +162,113 @@ $(document).ready(function () {
     var $marker = $(".marker.blueprint");
     var new_marker;
     for (var i in location_array) {
-      new_marker = $marker.clone();
-      new_marker.removeClass("blueprint");
-      new_marker.attr('id', location_array[i].name + "_marker");
-      var left = location_array[i].left.toString() + "%";
-      var top = location_array[i].top.toString() + "%";
-      new_marker.css("left", left);
-      new_marker.css("top", top);
-      $current_map.append(new_marker);
+      if (mills_array.indexOf(location_array[i].name) != -1) {
+        new_marker = $marker.clone();
+        new_marker.removeClass("blueprint");
+        new_marker.attr("id", location_array[i].name + "_marker");
+        var left = location_array[i].left.toString() + "%";
+        var top = location_array[i].top.toString() + "%";
+        new_marker.css("left", left);
+        new_marker.css("top", top);
+        $current_map.append(new_marker);
+      }
     }
     /**
      * Card active and inactive.
      */
-    $(".marker").bind("click", function () {
+    $(".marker").bind("click", function() {
       var t = $(this)[0];
       $(".card").addClass("active");
       $(".card").attr("id", t.id);
-      $(".marker").each(function () {
+      $(".marker").each(function() {
         if ($(this)[0] != t) {
           $(this).addClass("inactive");
         }
       });
       var element = $(".content-text")[0];
       var mill_name = t.id.replace("_marker", "");
-      console.log(mill_data);
       var a_mill = new_mill_data[mill_name];
-      console.log(a_mill);
+      var display_name = a_mill.display_name;
       var parish = a_mill.parish;
-      var extant = a_mill.extand_or_ruin;
+      var extant = a_mill.extant_or_ruin;
       var founding_date = a_mill.date_of_establishment;
       var lat = a_mill.lat;
       var long = a_mill.long;
-      element.innerHTML = "This mill was founded in <b>" + founding_date + "</b>. It belongs to <b>" + parish + "</b> parish, and it's precise GPS location is <b>" + long + "</b> longitude and <b>" + lat + "</b> latitude.";
+      $(".card .title")[0].innerHTML = display_name;
+      $(".card .subtitle")[0].innerHTML = extant;
+      element.innerHTML =
+        "This mill was founded in <b>" +
+        founding_date +
+        "</b>. It belongs to <b>" +
+        parish +
+        "</b> parish, and it's precise GPS location is <b>" +
+        long +
+        "</b> longitude and <b>" +
+        lat +
+        "</b> latitude.";
     });
   }
-
 
   // ------------------ MAP FUNCTIONS --------------------------
 
   /**
    * switch to different parishes on click
    */
-  $("#stjohn-path").on("click", function () {
+  $("#stjohn-path").on("click", function() {
     $current_map.fadeOut();
     $("#stjohn_map").fadeIn();
     $current_map = $("#stjohn_map");
     create_mill_marker(stjohn_mill_locations);
+    current_mills_array = mills_by_parishes_array["St.John"];
+    add_mills_to_menu();
   });
 
-  $("#stpaul-path").on("click", function () {
+  $("#stpaul-path").on("click", function() {
     $current_map.fadeOut();
     $("#stpaul_map").fadeIn();
     $current_map = $("#stpaul_map");
     create_mill_marker(stpaul_mill_locations);
+    add_mills_to_menu();
   });
 
-  $("#stpeter-path").on("click", function () {
+  $("#stpeter-path").on("click", function() {
     $current_map.fadeOut();
     $("#stpeter_map").fadeIn();
     $current_map = $("#stpeter_map");
     create_mill_marker(stpeter_mill_locations);
+    add_mills_to_menu();
   });
 
-  $("#stphilip-path").on("click", function () {
+  $("#stphilip-path").on("click", function() {
     $current_map.fadeOut();
     $("#stphilip_map").fadeIn();
     $current_map = $("#stphilip_map");
     create_mill_marker(stphilip_mill_locations);
+    add_mills_to_menu();
   });
 
-  $("#stmary-path").on("click", function () {
+  $("#stmary-path").on("click", function() {
     $current_map.fadeOut();
     $("#stmary_map").fadeIn();
     $current_map = $("#stmary_map");
     create_mill_marker(stmary_mill_locations);
+    add_mills_to_menu();
   });
 
-  $("#stgeorge-path").on("click", function () {
+  $("#stgeorge-path").on("click", function() {
     $current_map.fadeOut();
     $("#stgeorge_map").fadeIn();
     $current_map = $("#stgeorge_map");
+    current_mills_array = mills_by_parishes_array["St.George"];
     create_mill_marker(stgeorge_mill_locations);
+    add_mills_to_menu();
   });
 
   /**
    * What to do when map button is clicked.
    *
    */
-  $("#map_button").on("click", function () {
+  $("#map_button").on("click", function() {
     if ($current_map[0] != $("#antigua_map")[0]) {
       $current_map.fadeOut();
       $("#antigua_map").fadeIn();
@@ -261,13 +284,12 @@ $(document).ready(function () {
   var $card = $(".blueprint .card");
   var $modal = $(".blueprint .modal");
 
-
-  $(".card .button-secondary").on("click", function () {
+  $(".card .button-secondary").on("click", function() {
     $(".card").removeClass("active");
     $(".marker").removeClass("inactive");
   });
 
-  $(".card .button-primary").on("click", function () {
+  $(".card .button-primary").on("click", function() {
     show_full_info();
   });
 
@@ -279,31 +301,40 @@ $(document).ready(function () {
     var element = $(".card")[0];
     var mill_name = element.id.replace("_marker", "");
     var a_mill = new_mill_data[mill_name];
-    // var parish = a_mill.parish;
-    // var extant = a_mill.extand_or_ruin;
-    // var founding_date = a_mill.date_of_establishment;
-    // var lat = a_mill.lat;
-    // var long = a_mill.long;
-    // var description = a_mill.additional_info;
-    var parish = "St.John";
-    var extant = "what";
-    var founding_date = 1974;
-    var lat = 1234;
-    var long = 5678;
-    var description = "wow";
+    var a_mill = new_mill_data[mill_name];
+    var display_name = a_mill.display_name;
+    var parish = a_mill.parish;
+    var extant = a_mill.extant_or_ruin;
+    var founding_date = a_mill.date_of_establishment;
+    var lat = a_mill.lat;
+    var long = a_mill.long;
+    var description = a_mill.additional_info;
+    var chronology = a_mill.chronology;
+    // update name
+    $(".modal-title")[0].innerHTML = display_name;
     // update general info
-    $(".modal-genInfo")[0].innerHTML = "<b>Parish: </b>" + parish + " <br> <b>Founding date: </b>" + founding_date + "<br> <b>Long, lat: </b>" + long + "," + lat;
+    $(".modal-genInfo")[0].innerHTML =
+      "<b>Type: </b>" +
+      extant +
+      "<br> <b>Parish: </b>" +
+      parish +
+      " <br> <b>Founding date: </b>" +
+      founding_date +
+      "<br> <b>Long, lat: </b>" +
+      long +
+      ", " +
+      lat;
     // update description
     $(".modal-desc")[0].innerHTML = description;
     // update the chronology
-    $('.timeline').append(
-      $('<li>').attr({
-        "class": "event",
-        "data-date": "1777"
-      }).append(
-        $('<p>').text('what the hell')
-      )
-    );
+    console.log(chronology);
+    for (var year in chronology) {
+      $(".timeline").append(
+        $("<li>")
+          .attr({ class: "event", "data-date": year })
+          .append($("<p>").text(chronology[year]))
+      );
+    }
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
@@ -312,16 +343,14 @@ $(document).ready(function () {
     modal.css("transform", "translateY(0)");
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    span.onclick = function() {
       modal.css("transform", "translateY(100%)");
     };
   }
-
-
 });
 /**
  * Switch to another tab with tab_name in the modal
- * @param {*Ch} evt 
+ * @param {*Ch} evt
  * @param {*} tab_name: the tab to switch to
  */
 function change_tab(evt, tab_name) {
