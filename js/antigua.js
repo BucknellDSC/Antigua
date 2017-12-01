febe5d5fa5904f710a4c4a86ae804b2e09437dc0
khaiquangnguyen <kqn001@bucknell.edu>
2017-12-01 00:27:45 -0500

    done

--- js/antigua.js
+++ js/antigua.js
@@ -240,3 +240,17 @@ function show_full_info() {
     modal.css("transform", "translateY(100%)");
   };
 }
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
