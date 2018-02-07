e66a5a1f90bba839ee6a87d9fe36bfd09954fb46
Lina Computer <griot@bucknell.edu>
2018-02-07 15:20:53 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -421,7 +421,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Belvedere Plantation (a.k.a. Hornes).jpeg",
