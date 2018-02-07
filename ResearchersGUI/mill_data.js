eaf663ff0330e94ea5b33ce2a3c6efbf79851b8e
Lina Computer <griot@bucknell.edu>
2018-02-07 15:19:03 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -382,7 +382,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Allens Plantation (a.k.a. Mountains View).jpeg",
