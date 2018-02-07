5bd21a5cbd272d81a7d5ed9858d40126d42f389e
Lina Computer <griot@bucknell.edu>
2018-02-07 15:26:31 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -460,7 +460,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Boones.jpeg",
