6518538e7501c8f3a15121b5ecb4611b814b7c2c
Lina Computer <griot@bucknell.edu>
2018-02-07 21:45:52 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -746,7 +746,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Hey Hey it's the Monkees",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Hawkebills Plantation (a.k.a. Hansons).jpeg",
