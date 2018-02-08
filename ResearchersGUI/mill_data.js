529648336c48b8b1f85025a089c6d680f24878d2
Lina Computer <griot@bucknell.edu>
2018-02-07 21:41:14 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -733,7 +733,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Harts and Royals.jpeg",
