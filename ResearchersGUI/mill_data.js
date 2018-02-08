1ed6d666629a6745b91168cf4e7791628c14091f
Lina Computer <griot@bucknell.edu>
2018-02-07 21:31:36 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -538,7 +538,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Clare Hall.jpeg",
