b5bb302c450427c1aa447b33323eae3461546a7c
Lina Computer <griot@bucknell.edu>
2018-02-07 21:40:49 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -720,7 +720,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Hallidays Mountation (a.k.a. Gilleads, Gilliats, or Providence).jpeg",
