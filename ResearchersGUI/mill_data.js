dfc0ecb96afbb123d255435ee54044fda6ac821c
Lina Computer <griot@bucknell.edu>
2018-02-07 21:36:17 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -629,7 +629,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Giles Blizzard",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Five Islands Plantation (Upper and Lower Pelican).jpeg",
