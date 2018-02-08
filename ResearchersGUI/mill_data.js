c504264b53254f7f3041b1b08d963e10ca5f4a72
Lina Computer <griot@bucknell.edu>
2018-02-07 21:54:59 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -941,7 +941,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Hey Hey it's the Monkees",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/The Union Plantation (HaddonHatton or Weekes).jpeg",
