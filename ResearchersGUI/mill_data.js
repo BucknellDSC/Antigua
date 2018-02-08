13c68054b2887247368e251561b2ad81ab739759
Lina Computer <griot@bucknell.edu>
2018-02-07 21:52:25 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -876,7 +876,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Hey Hey it's the Monkees",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Potters Plantation.jpeg",
