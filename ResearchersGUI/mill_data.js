ed309b3e956154639beba7f91734320a296f8d79
Lina Computer <griot@bucknell.edu>
2018-02-08 11:29:42 -0500

    "Mill files updated"

--- ResearchersGUI/mill_data.js
+++ ResearchersGUI/mill_data.js
@@ -1227,7 +1227,7 @@ var mill_data = [
         "additional_info": "",
         "chronology": {},
         "date_of_establishment": "",
-        "display_name": "",
+        "display_name": "Hey Hey it's the Monkees",
         "enslaved_peoples": "",
         "extant_or_ruin": "",
         "image": "Mill_Files/Photos/Little Duers.jpeg",
